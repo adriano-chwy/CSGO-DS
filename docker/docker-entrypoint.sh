3773166f1091ba6feccc900af994c816d32213d6
asoares997 <adrianojsoares1@gmail.com>
2020-02-22 22:12:15 -0500

    small adjustments

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,16 +5,16 @@
 # Transfer context from shell to CS:GO Dedicated Server + Invoke the Source Engine Runner (Linux distributions only)
 exec /home/steam/SteamCLI/csgods/srcds_run \
     # Specify CS:GO as the current game
-    "-game csgo" \
+    -game csgo \
     # Enables Console Mode
-    "-console" \
+    -console \
     # Allows server admins to connect via RCON. Disable RCON by removing this flag (and +rcon_password below).
-    "-usercon" \
+    -usercon \
     # Steam Account ID (necessary to make this a public server register with Valve)
-    "+sv_setsteamaccount ${SRCDS_LOGIN_TOKEN}" \
+    +sv_setsteamaccount ${SRCDS_LOGIN_TOKEN} \
     # RCON password for remote access. If blank admin access is public! (Not recommended)
-    "+rcon_password ${SV_RCON_PASSWORD}" \
+    +rcon_password ${SV_RCON_PASSWORD} \
     # Password to enter the server. If blank the server has public access!
-    "+sv_password ${SV_PASSWORD}"
+    +sv_password ${SV_PASSWORD}
 
 # --- End Server Launch ---
